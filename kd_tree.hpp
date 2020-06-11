8a722cd152d8212be241c2a2541a761562505c69
Gabe Hart <flyingmessier@gmail.com>
2020-06-11 10:45:25 -0400

    modified kd_tree return types, improved mem efficiency

--- kd_tree.hpp
+++ kd_tree.hpp
@@ -11,12 +11,11 @@ template<size_t DIMS>
 class KDTree {
 private:
   struct KDNode {
-    PathTree<DIMS>* pt;
-    Point<DIMS> point;  // TODO: refactor to use point already stored in pt
+    PathTree<DIMS>& pt;
     KDNode* smaller;
     KDNode* greater;
     KDNode* parent;
-    KDNode(PathTree<DIMS>* n, KDNode* p): pt(n), point(n->point), parent(p), smaller(nullptr), greater(nullptr) {}
+    KDNode(PathTree<DIMS>& n, KDNode* p): pt(n), parent(p), smaller(nullptr), greater(nullptr) {}
     ~KDNode() {
       delete smaller;
       delete greater;
@@ -38,16 +37,16 @@ private:
   }
 
   KDNode* nearest_helper_(Point<DIMS> p, KDNode* r, size_t axis) const {
-    double d_r = dist_(p, r->point);
+    double d_r = dist_(p, r->pt.point);
     double d_s = 0;
     double d_g = 0;
     unsigned char children = 0; // 1s bit is presence of smaller child, 2s is greater
     if (r->smaller != nullptr) {
-      d_s = dist_(p, r->smaller->point);
+      d_s = dist_(p, r->smaller->pt.point);
       children += 1;
     }
     if (r->greater != nullptr) {
-      d_g = dist_(p, r->greater->point);
+      d_g = dist_(p, r->greater->pt.point);
       children += 2;
     }
     if (!children) {
@@ -55,14 +54,14 @@ private:
     }
     bool side = ((children == 3) && (d_s < d_g)) || (children == 2);  // true if closer to smaller, false if closer to greater
     KDNode* side_res = nearest_helper_(p, side ? r->smaller : r-> greater, (axis + 1) % DIMS);
-    double side_dist = dist_(p, side_res->point);
-    if (side_dist <= dist_axis_(p, r->point, axis)) {
+    double side_dist = dist_(p, side_res->pt.point);
+    if (side_dist <= dist_axis_(p, r->pt.point, axis)) {
       return side_res;
     } else if (children < 3) {
       return side_dist < d_r ? side_res : r;
     } else {
       KDNode* other_res = nearest_helper_(p, side ? r-> greater : r->smaller, (axis + 1) % DIMS);
-      double other_dist = dist_(p, other_res->point);
+      double other_dist = dist_(p, other_res->pt.point);
       unsigned char best = (side_dist < d_r && side_dist < other_dist) +
                            2 * (other_dist < d_r && other_dist < side_dist);  // 0 for local root, 1 for side, 2 for other
       switch (best) {
@@ -76,16 +75,16 @@ private:
     }
   }
 
-  void near_helper_(Point<DIMS> p, double radius, std::vector<Point<DIMS> >& v, KDNode* r, size_t axis) const {
+  void near_helper_(Point<DIMS> p, double radius, std::vector<PathTree<DIMS>& >& v, KDNode* r, size_t axis) const {
     bool continue_s = false;
     bool continue_g = false;
-    if (dist_(p, r->point) < radius) {
-      v.push_back(r->point);
+    if (dist_(p, r->pt.point) < radius) {
+      v.push_back(r->pt);
     }
-    if (dist_axis_(p, r->point, axis) < radius) {
+    if (dist_axis_(p, r->pt.point, axis) < radius) {
       continue_s = true;
       continue_g = true;
-    } else if (p[axis] > r->point[axis]) {
+    } else if (p[axis] > r->pt.point[axis]) {
       continue_g = true;
     } else {
       continue_s = true;
@@ -103,7 +102,7 @@ public:
     root_ = nullptr;
   }
 
-  KDTree(PathTree<DIMS>* root) {
+  KDTree(PathTree<DIMS>& root) {
     root_ = new KDNode(root, nullptr);
   }
 
@@ -112,15 +111,14 @@ public:
   }
 
 
-  void insert(PathTree<DIMS>* pt) {
-    Point<DIMS> p = pt->point;
+  void insert(PathTree<DIMS>& p) {
     KDNode* parent = root_;
     size_t axis = 0;
     while (true) {
-      KDNode* branch = (parent->point)[axis] < p[axis] ? parent->greater : parent->smaller;
+      KDNode* branch = (parent->pt.point)[axis] < p.point[axis] ? parent->greater : parent->smaller;
       if (branch == nullptr) {
-        ((parent->point)[axis] < p[axis] ? parent->greater : parent->smaller) =
-          new KDNode(pt, parent);
+        ((parent->pt.point)[axis] < p.point[axis] ? parent->greater : parent->smaller) =
+          new KDNode(p, parent);
         return;
       } else {
         parent = branch;
@@ -130,11 +128,11 @@ public:
     }
   }
 
-  Point<DIMS> nearest(Point<DIMS> p) const {
-    return nearest_helper_(p, root_, 0)->point;
+  PathTree<DIMS>& nearest(Point<DIMS> p) const {
+    return nearest_helper_(p, root_, 0)->pt;
   }
 
-  Point<DIMS> remove(Point<DIMS> p) {
+  void remove(Point<DIMS> p) {
     KDNode* near = nearest_helper_(p, root_, 0);
     KDNode* parent = near->parent;
     bool smaller = parent->smaller == near;
@@ -143,25 +141,25 @@ public:
   }
 
   // returns vector where the first element is the start and the last is the goal
-  std::vector<Point<DIMS> > pathToNearest(Point<DIMS> p) const {
-    std::vector<Point<DIMS> > res;
-    KDNode* n = nearest(p);
-    while (n != nullptr) {
-      res.push_back(n->point);
-      n = n->parent;
-    }
-    std::vector<Point<DIMS> > r_res;
-    for (typename std::vector<Point<DIMS> >::reverse_iterator it = res.rbegin(); it != res.rend(); it++) {
-      r_res.push_back(*it);
-    }
-    return r_res;
-  }
+  // is working, but is not needed
+  // std::vector<Point<DIMS> > pathToNearest(Point<DIMS> p) const {
+  //   std::vector<Point<DIMS> > res;
+  //   KDNode* n = nearest(p);
+  //   while (n != nullptr) {
+  //     res.push_back(n->pt.point);
+  //     n = n->parent;
+  //   }
+  //   std::vector<Point<DIMS> > r_res;
+  //   for (typename std::vector<Point<DIMS> >::reverse_iterator it = res.rbegin(); it != res.rend(); it++) {
+  //     r_res.push_back(*it);
+  //   }
+  //   return r_res;
+  // }
 
   // all nodes within radius r of p
-  std::vector<Point<DIMS> > near(Point<DIMS> p, double r) const {
+  std::vector<PathTree<DIMS>& > near(Point<DIMS> p, double r) const {
     std::vector<Point<DIMS> > res;
     return near_helper_(p, r, res, root_, 0);
-    return res;
   }
 };
 
